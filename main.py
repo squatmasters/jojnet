bc14c46f3e66e0abf4bd412550597215c45d292e
spencer <spencer@speherh.com>
2017-05-17 14:59:48 -0400

    basic about text

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def showUser(username):
   return render_template('profile.html', un=username)
 
 @app.route("/about/")
-def aboub():
-  return "jojent is hepatitis c"
+def about():
+  return "JOJNET is the hip new social network."
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
