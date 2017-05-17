719010b69f0480b42bc83cb9c8a46ddc33d54059
spencer <spencer@speherh.com>
2017-05-17 15:22:52 -0400

    scss stuffs

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+url_for('static', 'main.css')
 
 @app.route("/")
 def hello():
   return "jojnet"
 
+@app.route("/user/<username>")
+def showUser(username):
+  return render_template('profile.html', un=username)
+
+@app.route("/about/")
+def about():
+  return "JOJNET is the hip new social network."
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=80)
+  app.run(host='0.0.0.0')
